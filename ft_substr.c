833e1f8ec1773c0efd1c5aba3da4e7fa2091c68a
Farsana <124598551+farsanasakkeer@users.noreply.github.com>
2024-08-20 10:45:41 +0400

    Create ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	s_len;
+
+	if (s == NULL)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+		substr = malloc(1);
+		if (substr == NULL)
+			return (NULL);
+		substr[0] = '\0';
+		return (substr);
+	}
+	if (len > s_len - start)
+		len = s_len - start;
+	substr = ft_calloc(len + 1, sizeof(char));
+	if (substr == NULL)
+		return (NULL);
+	ft_memcpy(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
